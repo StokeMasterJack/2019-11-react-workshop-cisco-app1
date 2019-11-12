53464134459ada7429001647f72df16385383689
David Ford <dford@smart-soft.com>
2019-11-12 15:07:32 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Red({suffix, bg}) {
+    const bg1 = bg ? bg : 'yellow';
+    const suffix1 = suffix ? suffix : ' MD';
+
+    const bg2 = bg || 'pink';
+    return <div style={{color: 'red', backgroundColor: bg2}}>
+        Red {suffix1}
+    </div>;
+}
+
+function Blue() {
+    return <div style={{color: 'blue', backgroundColor: '#EEEEEE'}}>
+        Blue
+    </div>;
+}
+
+function ChosePage({pageName}) {
+    if (pageName === 'redPage') {
+        return <div>
+            <h1>Red Page</h1>
+            <Red bg='lightGreen' suffix='DDS'/>
+            <Red bg={'cyan'} suffix='Dude Man'/>
+            <Red/>
+        </div>;
+    } else if (pageName === 'bluePage') {
+        return <div>
+            <h1>Blue Page</h1>
+            <Blue/>
+            <Blue/>
+            <Blue/>
+        </div>;
+    } else {
+        return <div>Bad pageName {pageName}</div>;
+    }
+
+}
+
+function CButton({children}) {
+    return <button style={{margin: '.5rem', color: 'darkRed'}}>{children}</button>;
+}
+
+function ButtonBar() {
+
+    const style = {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        position: 'absolute',
+        margin: 0,
+        backgroundColor: '#CCCCCC'
+    };
+
+    return <div style={style}>
+        <CButton>Red</CButton>
+        <CButton>Blue</CButton>
+    </div>;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <div>
+        App1
+        <div>
+            <ChosePage pageName={'bluePage'}/>
+            <ButtonBar/>
+        </div>
+    </div>;
 }
+
 
 export default App;
